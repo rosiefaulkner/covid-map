6ef0f91099f1441857d15ef40eae15b3b8249d0b
Rosie Faulkner <rosiefaulkner@0069l2z7crmlcvf.lan>
2021-02-08 00:55:45 -0500

    renders world map from leaflet dependency

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,22 +3,19 @@ import ReactDOM from 'react-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './css/Leaflet.css';
 
-const position = [39.8283, -98.5795]
-
-
 export function Map() {
+    const position = [39.8283, -98.5795]
     return (
         <MapContainer 
-        center={[43.38621, -79.83713]} 
-        zoom="4" 
+        center={position} 
+        zoom="3" 
         scrollWheelZoom={false}
-        style={{height: "100vh"}}
+        style={{height: "50vh"}}
         >
         <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
         </MapContainer>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
 import {Map} from './components/Map';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
